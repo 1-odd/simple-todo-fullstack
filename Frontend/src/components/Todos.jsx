0a7e95402afcf00c84036b883ff1ef28f04f3da7
Ritik Kumar <ritikraj2018634@gmail.com>
2024-03-28 03:03:13 +0530

    update gitignore

--- Frontend/src/components/Todos.jsx
+++ Frontend/src/components/Todos.jsx
@@ -5,12 +5,26 @@ export function Todos({ todos }) {
     return (
         <div>
             {todos.map((todo) => (
+                console.log(todo._id), 
                 <div key={todo.id}>
                     <h1>{todo.title}</h1>
                     <h2>{todo.description}</h2>
-                    <button>{todo.completed ? "completed" : "mark as complete"}</button>
+                    <button onClick={()=>{
+                        fetch("http://localhost:3001/todo-completed",{
+                            method:"PUT",
+                            body:JSON.stringify({
+                                id: todo._id,
+                            }),
+                            headers:{ "Content-Type": "application/json"}
+                            
+                        }).then(async (res)=>{
+                            const  data = await res.json();
+                            alert(data.msg);
+                        })
+                    }}>{todo.completed ? "completed" : "mark as complete"}</button>
                 </div>
             ))}
         </div>
     );
 }
+
--- README.md
+++ README.md
@@ -5,3 +5,36 @@ It has the following features:
 - Anyone  can create a todo
 - Anyone can see their existing todos
 - Anyone can mark a todo as done
+
+**Follow these instruction to run application**
+- create **.env** file in the backend folder and add these 2 things
+	-  PORT = 3001
+	- DB_URL = your MongoDB connection String till .net only
+- run this command inside backend folder in your terminal 
+ 
+       npm i
+       npm start
+
+- now your backend is ready move to frontend folder
+- run these command in Frontend folder 
+
+	    npm i
+	    npm run dev
+
+   
+ - Now your application is in working mode .
+ - if you face any issue read it out again or can contact with me.
+
+
+**Tech-stack we used :**
+ 1. Backend:
+	 - Node , Express , MongoDB , zod ( for payload validation ) etc.
+2. Frontend:
+	- React only 
+
+**Improvements**
+
+ - Can do lots of improvements
+	 -  Can add Auth.. System
+	 - Can add a delete todo logic 
+	 - Can use better logic to improve re-rendering performance.
